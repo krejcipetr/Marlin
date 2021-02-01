0fb56ec52dbbf98b8dd0371ce7dcba9bbce0c98a
Petr Krejci <petr.krejci@it-g.cz>
2021-02-01 19:47:31 +0100

    nastaveni

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -563,7 +563,7 @@
  #define DEFAULT_bedKi 4.63
  #define DEFAULT_bedKd 111.50
 
-  //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
+  //120V 250W silicone heater into 4mm borosilicate (MendelMa/home/krejci/workspacex 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   //#define DEFAULT_bedKp 10.00
   //#define DEFAULT_bedKi .023
