4eefaf9d7c9ea82ac7e03e0ca738f746192256a0
Petr Krejci <petr.krejci@it-g.cz>
2024-10-26 22:15:57 +0200

    cidlo teploty z druheho vstupu

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
@@ -95,8 +95,8 @@
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN                          PC1   // TH1
-#define TEMP_1_PIN                          PC2   // TH2
+#define TEMP_0_PIN                          PC2   // TH1
+//#define TEMP_1_PIN                          PC2   // TH2
 #define TEMP_BED_PIN                        PC0   // TB1
 
 //
